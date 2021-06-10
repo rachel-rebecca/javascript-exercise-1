4d488bc73bf4e0db9fe0c1fdaaf9c3b223872dc6
rachel-rebecca <Rarebecc@umich.edu>
2021-06-09 21:34:27 -0400

    prettier update

--- script.js
+++ script.js
@@ -2,30 +2,30 @@ let actualTemp = prompt("What's the actual temperature?");
 let desiredTemp = 72;
 
 if (actualTemp < desiredTemp) {
-    alert("Run heat")
-    console.log("Run heat")
+  alert("Run heat");
+  console.log("Run heat");
 } else if (actualTemp > desiredTemp) {
-    alert("Run A/C")
-    console.log("Run A/C")
+  alert("Run A/C");
+  console.log("Run A/C");
 } else {
-    alert("Standby")
-    console.log("Standby")
+  alert("Standby");
+  console.log("Standby");
 }
 
-let tempCelsius = prompt("Temperature in Celsius to convert:")
+let tempCelsius = prompt("Temperature in Celsius to convert:");
 
-const targetUnit = prompt("Select target unit: F, C, or K")
+const targetUnit = prompt("Select target unit: F, C, or K");
 switch (targetUnit) {
-    case "F":
-       let double = tempCelsius * 2
-       let final = double + 30;
-       console.log(final + " F")
-       break;
-    case "C":
-        console.log(tempCelsius + " C")
-        break;
-    case "K":
-        let kelvin = tempCelsius + 273.15;
-        console.log(kelvin + " K");
-        break;
+  case "F":
+    let double = tempCelsius * 2;
+    let final = double + 30;
+    console.log(final + " F");
+    break;
+  case "C":
+    console.log(tempCelsius + " C");
+    break;
+  case "K":
+    let kelvin = tempCelsius + 273.15;
+    console.log(kelvin + " K");
+    break;
 }
